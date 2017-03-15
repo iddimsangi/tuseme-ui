1d2cd21136d250690f7c78665756b1ec25a3a784
mkumbobeaty <charsbeaty@gmail.com>
2017-03-15 14:09:40 +0300

    modification

--- src/app/kiongozi/header/header.component.ts
+++ src/app/kiongozi/header/header.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  isIn = false;   // store state
+  toggleState() { // click handler
+    let bool = this.isIn;
+    this.isIn = bool === false ? true : false;
+  }
   constructor() { }
 
   ngOnInit() {

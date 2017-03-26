5148cc5a1008659c6e76c851c7f420c402c2eaf9
Mloweedgar <mloweedgar0@gmail.com>
2017-03-26 17:35:32 +0300

    changes

--- src/app/kiongozi/kiongozi-routing.module.ts
+++ src/app/kiongozi/kiongozi-routing.module.ts
@@ -6,11 +6,17 @@ import { KiongoziComponent } from './kiongozi/kiongozi.component';
 import { MarejeshoHomeComponent } from './marejesho-home/marejesho-home.component';
 import { TumaHomeComponent } from './tuma-home/tuma-home.component';
 import { DuaHomeComponent } from './dua-home/dua-home.component';
+import {TumaMapendekezoComponent} from "./tuma/tuma-mapendekezo/tuma-mapendekezo.component";
+import {TumaRipotiComponent} from "./tuma/tuma-ripoti/tuma-ripoti.component";
+import {TumaTaarifaComponent} from "./tuma/tuma-taarifa/tuma-taarifa.component";
 
 const routes: Routes = [
    { path: '', component: KiongoziComponent,
    children: [
      { path: '',   redirectTo: 'tuma', pathMatch: 'full' },
+     {path:'tmaTaarifa', component:TumaTaarifaComponent},
+     {path:'tumaRipoti', component:TumaRipotiComponent},
+     {path:'tumaMapendekezo', component:TumaMapendekezoComponent},
      { path: 'tuma', component: TumaHomeComponent },
      { path: 'dua', component: DuaHomeComponent },
      { path: 'marejesho', component: MarejeshoHomeComponent }

678e9e7ab1ab8c9e344e4aac9ddcdd2ceb5f060f
Mloweedgar <mloweedgar0@gmail.com>
2017-03-27 15:03:59 +0300

    modifying links

--- src/app/kiongozi/kiongozi-routing.module.ts
+++ src/app/kiongozi/kiongozi-routing.module.ts
@@ -9,15 +9,21 @@ import { DuaHomeComponent } from './dua-home/dua-home.component';
 import {TumaMapendekezoComponent} from "./tuma/tuma-mapendekezo/tuma-mapendekezo.component";
 import {TumaRipotiComponent} from "./tuma/tuma-ripoti/tuma-ripoti.component";
 import {TumaTaarifaComponent} from "./tuma/tuma-taarifa/tuma-taarifa.component";
+import {ContentsComponent} from "./tuma/contents/contents.component";
 
 const routes: Routes = [
    { path: '', component: KiongoziComponent,
    children: [
      { path: '',   redirectTo: 'tuma', pathMatch: 'full' },
-     {path:'tmaTaarifa', component:TumaTaarifaComponent},
-     {path:'tumaRipoti', component:TumaRipotiComponent},
-     {path:'tumaMapendekezo', component:TumaMapendekezoComponent},
-     { path: 'tuma', component: TumaHomeComponent },
+     { path: 'tuma', component: TumaHomeComponent,
+
+     children:[
+       {path:'', component:ContentsComponent},
+       {path:'taarifa', component:TumaTaarifaComponent},
+       {path:'ripoti', component:TumaRipotiComponent},
+       {path:'mapendekezo', component:TumaMapendekezoComponent},
+     ]
+     },
      { path: 'dua', component: DuaHomeComponent },
      { path: 'marejesho', component: MarejeshoHomeComponent }
    ]
--- src/app/kiongozi/tuma/tuma.module.ts
+++ src/app/kiongozi/tuma/tuma.module.ts
@@ -6,13 +6,14 @@ import { TumaTaarifaComponent } from './tuma-taarifa/tuma-taarifa.component';
 import { TumaRipotiComponent } from './tuma-ripoti/tuma-ripoti.component';
 import { TumaUjumbeComponent } from './tuma-ujumbe/tuma-ujumbe.component';
 import { TumaMapendekezoComponent } from './tuma-mapendekezo/tuma-mapendekezo.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TumaRoutingModule
   ],
-  declarations: [ContentsComponent, TumaTaarifaComponent, TumaRipotiComponent, TumaUjumbeComponent, TumaMapendekezoComponent],
+  declarations: [ContentsComponent, TumaTaarifaComponent, TumaRipotiComponent, TumaUjumbeComponent, TumaMapendekezoComponent, MainContentComponent],
   exports: [ContentsComponent]
 })
 export class TumaModule { }

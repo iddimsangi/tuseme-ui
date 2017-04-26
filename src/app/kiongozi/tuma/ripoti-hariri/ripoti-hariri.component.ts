69b09baeb847eacf5cacf2dfd796da6b5474ac42
Mloweedgar <mloweedgar0@gmail.com>
2017-04-26 14:27:04 +0300

    new components

---
+++ src/app/kiongozi/tuma/ripoti-hariri/ripoti-hariri.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ripoti-hariri',
+  templateUrl: './ripoti-hariri.component.html',
+  styleUrls: ['./ripoti-hariri.component.css']
+})
+export class RipotiHaririComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/kiongozi/tuma/tuma.module.ts
+++ src/app/kiongozi/tuma/tuma.module.ts
@@ -9,13 +9,16 @@ import { TumaMapendekezoComponent } from './tuma-mapendekezo/tuma-mapendekezo.co
 import { TaarifaZilizotumwaComponent } from './taarifa-zilizotumwa/taarifa-zilizotumwa.component';
 import { TaarifaHaririComponent } from './taarifa-hariri/taarifa-hariri.component';
 import { AndikaTaarifaComponent } from './andika-taarifa/andika-taarifa.component';
+import { RipotiHaririComponent } from './ripoti-hariri/ripoti-hariri.component';
+import { RipotiZilizotumwaComponent } from './ripoti-zilizotumwa/ripoti-zilizotumwa.component';
+import { AndikaZilizotumwaComponent } from './andika-zilizotumwa/andika-zilizotumwa.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TumaRoutingModule
   ],
-  declarations: [ContentsComponent, TumaTaarifaComponent, TumaRipotiComponent, TumaUjumbeComponent, TumaMapendekezoComponent, TaarifaZilizotumwaComponent, TaarifaHaririComponent, AndikaTaarifaComponent],
+  declarations: [ContentsComponent, TumaTaarifaComponent, TumaRipotiComponent, TumaUjumbeComponent, TumaMapendekezoComponent, TaarifaZilizotumwaComponent, TaarifaHaririComponent, AndikaTaarifaComponent, RipotiHaririComponent, RipotiZilizotumwaComponent, AndikaZilizotumwaComponent],
   exports: [ContentsComponent]
 })
 export class TumaModule { }

15132aeb7de6660906d089a6b1ddab3857bbe5e7
mkumbobeaty <charsbeaty@gmail.com>
2017-06-29 16:30:29 +0300

    changes

--- src/app/core/models/report.ts
+++ src/app/core/models/report.ts
@@ -2,7 +2,8 @@ export class Report {
   id: number;
   title: string;
   description: string;
-  user_id:number;
-  street_id:number;
+  user_id: number;
+  street_id: number;
   created_at: any;
+  position_name: string;
 }
--- src/app/mwananchi/ripoti/contents/contents.component.ts
+++ src/app/mwananchi/ripoti/contents/contents.component.ts
@@ -3,6 +3,7 @@ import {Report} from '../../../core/models/report';
 import {ReportService} from '../../../core/report.service';
 import { SessionService } from '../../../core/session.service';
 import { User } from '../../../core/models/user';
+import {Street} from "../../../core/models/street";
 
 @Component({
   selector: 'ripoti-contents',
@@ -11,22 +12,29 @@ import { User } from '../../../core/models/user';
 })
 export class ContentsComponent implements OnInit {
 reports: Report[];
-user:User = this.sessionService.getCurrentUser().user;
-  constructor(private ripotiservice: ReportService,private sessionService:SessionService) { }
+user: User = this.sessionService.getCurrentUser().user;
+street: Street;
+position: any;
+  constructor(private ripotiservice: ReportService, private sessionService: SessionService) { }
 
-  getReports(id:number){
+  getReports(id: number) {
     return this.ripotiservice.getStreetReports(id)
       .then(
-        res=> {
-          this.reports =res;
+        res => {
+          this.reports = res;
           console.log(res);
-          console.log('reports retrived')
+          console.log('reports retrived');
         }
-      )
+      );
   }
-
+getInfo() {
+    let info = this.sessionService.getCurrentUser();
+    this.street = info.street;
+    this.position = info.position;
+}
   ngOnInit() {
     this.getReports(this.user.street_id);
+    this.getInfo();
   }
 
 }
